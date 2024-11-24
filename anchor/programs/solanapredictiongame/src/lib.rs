1e976ffb7f5ccf490410ffabd678e213df7170ad
mazdaha <mazda.hamdi@gmail.com>
2024-11-24 19:47:02 +0100

    Clean anchor template

--- anchor/programs/solanapredictiongame/src/lib.rs
+++ anchor/programs/solanapredictiongame/src/lib.rs
@@ -2,69 +2,10 @@
 
 use anchor_lang::prelude::*;
 
-declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
+declare_id!("DFzXN3Xv5kUd7wuLwEDHDAdktEfcXpS4T6a9YAuk511y");
 
 #[program]
-pub mod solanapredictiongame {
+pub mod solana_prediction_game {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseSolanapredictiongame>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.solanapredictiongame.count = ctx.accounts.solanapredictiongame.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.solanapredictiongame.count = ctx.accounts.solanapredictiongame.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeSolanapredictiongame>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.solanapredictiongame.count = value.clone();
-    Ok(())
-  }
-}
-
-#[derive(Accounts)]
-pub struct InitializeSolanapredictiongame<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Solanapredictiongame::INIT_SPACE,
-  payer = payer
-  )]
-  pub solanapredictiongame: Account<'info, Solanapredictiongame>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseSolanapredictiongame<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub solanapredictiongame: Account<'info, Solanapredictiongame>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub solanapredictiongame: Account<'info, Solanapredictiongame>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Solanapredictiongame {
-  count: u8,
 }
---
+++ anchor/target/types/solana_prediction_game.ts
@@ -0,0 +1,16 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/solana_prediction_game.json`.
+ */
+export type SolanaPredictionGame = {
+  "address": "DFzXN3Xv5kUd7wuLwEDHDAdktEfcXpS4T6a9YAuk511y",
+  "metadata": {
+    "name": "solanaPredictionGame",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": []
+};
